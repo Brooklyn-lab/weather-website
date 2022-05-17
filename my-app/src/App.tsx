307ddfc831805295c1019a38879b3834a1e33d8b
Brooklyn-lab <stepanyukilja@gmail.com>
2022-05-17 19:02:11 +0300

    add new project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello, <span>World!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
